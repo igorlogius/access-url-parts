784b1c301178e5ffa75f3350b96e62f2a7309d7e
igorlogius <67047467+igorlogius@users.noreply.github.com>
2024-11-28 19:32:37 +0100

    chore: cleanup and remove some stuff likely nobody uses

--- popup.js
+++ popup.js
@@ -1,29 +1,37 @@
 /* global browser */
 
-const pc = document.querySelector("#parts-container");
+let counter = 1;
 
-let counter = 0;
-
-function addPart(text, href) {
-  const a = document.createElement("a");
-  a.setAttribute("href", href);
-  a.setAttribute("short", counter);
-  a.setAttribute("target", "_blank");
-  const div = document.createElement("div");
-  div.setAttribute("class", "part");
-  if (counter < 10) {
-    div.textContent = text; // + " |" + counter + "|";
-  } else {
-    div.textContent = text;
+document.addEventListener("DOMContentLoaded", async function () {
+  function addPart(text, href) {
+    const pc = document.querySelector("#parts-container");
+    const a = document.createElement("a");
+    a.setAttribute("href", href);
+    a.setAttribute("short", counter);
+    a.setAttribute("target", "_blank");
+    const div = document.createElement("div");
+    div.setAttribute("class", "part");
+    if (counter < 10) {
+      div.textContent = text; // + " |" + counter + "|";
+    } else {
+      div.textContent = text;
+    }
+    a.appendChild(div);
+    pc.appendChild(a);
+    counter++;
   }
-  a.appendChild(div);
-  pc.appendChild(a);
-  counter++;
-}
 
-(async function init() {
   try {
-    const url = new URL(await browser.runtime.sendMessage("url"));
+    const tabs = await browser.tabs.query({
+      currentWindow: true,
+      active: true,
+    });
+    const url = new URL(tabs[0].url);
+
+    if (url.origin === "null") {
+      window.close();
+      return;
+    }
     let parts = url.pathname.split("/");
     let joined = "";
 
@@ -37,27 +45,16 @@ function addPart(text, href) {
       }
       parts.shift();
     }
+
+    document.addEventListener("keydown", function (event) {
+      if (/[1-9]/.test(event.key)) {
+        const el = document.querySelector('a[short="' + event.key + '"]');
+        if (el !== null) {
+          el.click();
+        }
+      }
+    });
   } catch (e) {
     console.error(e.toString());
-  }
-})();
-
-document.addEventListener("keydown", function (event) {
-  switch (event.key) {
-    case "0":
-    case "1":
-    case "2":
-    case "3":
-    case "4":
-    case "5":
-    case "6":
-    case "7":
-    case "8":
-    case "9":
-      //console.debug("event.key", event.key);
-      document.querySelector('a[short="' + event.key + '"]').click();
-      break;
-    default:
-      break;
   }
 });
